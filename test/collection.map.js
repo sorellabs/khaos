8e16c92d0ccc79dbb98cdab017c55f8443113b1f
Quildreen Motta <quildreen@gmail.com>
2011-12-14 22:43:00 -0200

    Writes the missing tests for the collection::map module.

--- test/collection.map.js
+++ test/collection.map.js
@@ -15,10 +15,21 @@ function count() {
 describe('Package: collection', function() {
   describe('Module: map', function() {
     var foo = {a:1}
-    var data
+    var data, keys, values
+
+    function check_call(value, key) {
+      var i = keys.indexOf(key)
+      i.should.not.equal(-1)
+      values[i].should.equal(value)
+      keys.splice(i, 1)
+      values.splice(i, 1)
+      return [key, value] }
+      
 
     beforeEach(function() {
-      data = Object.create(foo, {
+      keys   = ['b', 'd']
+      values = [2, 4]
+      data   = Object.create(foo, {
         b: d(2),
         c: d(3, false),
         d: d(4) })})
@@ -32,8 +43,8 @@ describe('Package: collection', function() {
       it('- Should pass (value, key, map) to the iterator', function() {
         map.each(data, function(v,k,m) {
           m.should.equal(data)
-          ;['b', 'd'].should.contain(k)
-          ;[2, 4].should.contain(v) })})
+          keys.should.contain(k)
+          values.should.contain(v) })})
     })
 
     describe('at', function() {
@@ -97,13 +108,13 @@ describe('Package: collection', function() {
     describe('keys', function() {
       it('- Should return all enumerable keys in the map', function() {
         map.keys(null).length.should.equal(0)
-        map.keys(data).should.eql(['b','d']) })
+        map.keys(data).should.eql(keys) })
     })
 
     describe('values', function() {
       it('- Should return all enumerable values in the map', function() {
         map.values(null).length.should.equal(0)
-        map.values(data).should.eql([2, 4]) })
+        map.values(data).should.eql(values) })
     })
 
     describe('items', function() {
@@ -113,31 +124,73 @@ describe('Package: collection', function() {
     })
 
     describe('reduce', function() {
-      it('- Should apply the function to all key/value pairs in the map')
-      it('- Should return the accumulated value')
-      it('- Should use the first key/value pair as initial value if one isnt given')
+      it('- Should apply the function to all key/value pairs in the map', function() {
+        map.reduce(data, 0, function(acc, value, key) {
+          check_call(value, key) })
+
+        keys.should.be.empty
+        values.should.be.empty })
+
+      it('- Should return the accumulated value', function() {
+        map.reduce(data, 0, function(acc, value) {
+          return acc + value }).should.equal(6) })
+
+      it('- Should use the first key/value pair as initial value if one isnt given', function() {
+        map.reduce(data, function(acc, value) {
+          values.should.contain(acc) })})
     })
 
     describe('every', function() {
-      it('- Should check if all key/value pairs pass the predicate')
-      it('- Should short-circuit')
+      it('- Should check if all key/value pairs pass the predicate', function() {
+        map.every(data, check_call).should.be.true
+        keys.should.be.empty
+        values.should.be.empty })
+      
+      it('- Should short-circuit', function() {
+        var x = true
+        var c = 0
+        map.every(data, function(acc, value) {
+          c++
+          return x = !x }).should.be.false
+        c.should.equal(1) })
     })
 
     describe('some', function() {
-      it('- Should check if any key/value pairs pass the predicate')
-      it('- Should short-circuit')
+      it('- Should check if any key/value pairs pass the predicate', function() {
+        map.some(data, function(v,k){ check_call(v, k) }).should.be.false
+        keys.should.be.empty
+        values.should.be.empty })
+
+      it('- Should short-circuit', function() {
+        map.some(data, check_call).should.be.true
+        keys.length.should.equal(1) })
     })
 
     describe('filter', function() {
-      it('- Should keep only key/value pairs that pass the predicate')
-      it('- Should return a new map')
-      it('- Should keep only own/enumerable properties')
+      it('- Should keep only key/value pairs that pass the predicate', function() {
+        var m = map.filter(data, function(v, k) { return k == 'b' })
+        m.should.eql({'b': 2}) })
+
+      it('- Should return a new map', function() {
+        map.filter(data, check_call).should.not.equal(data) })
+
+      it('- Should keep only own/enumerable properties', function() {
+        var m = map.filter(data, function(v, k) { return k == 'c' })
+        m.should.eql({}) })
     })
 
     describe('map', function() {
-      it('- Should transform all enumerable key/value pairs')
-      it('- Should return a new map')
-      it('- Should keep only own/enumerable properties')
+      it('- Should transform all enumerable key/value pairs', function() {
+        var m = map.map(data, function(v, k) { return v * 2 })
+        m.b.should.equal(4)
+        m.d.should.equal(8) })
+
+      it('- Should return a new map', function() {
+        var m = map.map(data, check_call).should.not.equal(data) })
+
+      it('- Should keep only own/enumerable properties', function() {
+        var m = map.map(data, function(v, k){ return k == 'c'? v * 2 : v })
+        should.not.exist(m.c) })
     })
   })
 })
