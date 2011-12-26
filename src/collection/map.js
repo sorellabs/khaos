17f766c692341682235b53e116cb357b88270698
Quildreen Motta <quildreen@gmail.com>
2011-12-26 00:27:02 -0200

    Minor style changes.
    - Place variables at the top, but below the parameter list.
    - use module.exports.
    - Clean extraneous whitespace.

--- src/collection/map.js
+++ src/collection/map.js
@@ -26,7 +26,7 @@ var _keys        = Object.keys
   , clone        = Object.create
   , proto        = Object.getPrototypeOf
   , has_own_prop = {}.hasOwnProperty
-  
+
 
 
 //// - Traversable
@@ -34,7 +34,8 @@ var _keys        = Object.keys
 // Applies the iterator function to each key/value pair in the map.
 //
 // each :: {k -> e}, (e, k, {k -> e} -> Ignored) -> Undefined
-function each(map, iterator) { var i, len, props
+function each(map, iterator) {
+  var i, len, props
   props = keys(map)
   for (i = 0, len = props.length; i < len; ++i)
     iterator(map[props[i]], props[i], map) }
@@ -141,7 +142,8 @@ function items(map) {
 //
 // reduce :: {k -> e}, Any, (Any, e, k -> Any) -> Any
 // reduce :: {k -> e}, (Any, e, k -> Any) -> Any
-function reduce(map, value, folder) { var props, i, len
+function reduce(map, value, folder) {
+  var props, i, len
   map   = Object(map)
   props = keys(map)
   if (arguments.length == 2) {
@@ -158,7 +160,8 @@ function reduce(map, value, folder) { var props, i, len
 // Does the predicate succeeds for each key/value pair in the map?
 //
 // every :: {k -> e}, (e, k, {k -> e} -> Bool) -> Bool
-function every(map, predicate) { var i, props
+function every(map, predicate) {
+  var i, props
   map   = Object(map)
   props = keys(map)
   i     = props.length
@@ -171,7 +174,8 @@ function every(map, predicate) { var i, props
 // Does the predicate succeeds for any key/value pair in the map?
 //
 // some :: {k -> e}, (e, k, {k -> e} -> Bool) -> Bool
-function some(map, predicate) { var i, props
+function some(map, predicate) {
+  var i, props
   map   = Object(map)
   props = keys(map)
   i     = props.length
@@ -188,7 +192,8 @@ function some(map, predicate) { var i, props
 // properties are discarded.
 //
 // filter :: {k -> e}, (e, k, {k -> e} -> Bool) -> {k -> e}
-function filter(map, predicate) { var i, result, key, props
+function filter(map, predicate) {
+  var i, result, key, props
   props  = keys(map)
   i      = props.length
   result = clone(proto(map))
@@ -206,7 +211,8 @@ function filter(map, predicate) { var i, result, key, props
 // properties are discarded.
 //
 // map :: {k -> e}, (e, k, {k -> e} -> e) -> {k -> e}
-function map(map, mapper) { var i, result, key, props
+function map(map, mapper) {
+  var i, result, key, props
   props  = keys(map)
   i      = props.length
   result = clone(proto(map))
@@ -217,19 +223,20 @@ function map(map, mapper) { var i, result, key, props
 
 
 /// -Exports
-exports.each      = each
-exports.at        = at
-exports.put       = put
-exports.remove    = remove
-exports.clear     = clear
-exports.size      = size
-exports.empty_p   = empty_p
-exports.has_key_p = has_key_p
-exports.keys      = keys
-exports.values    = values
-exports.items     = items
-exports.reduce    = reduce
-exports.every     = every
-exports.some      = some
-exports.filter    = filter
-exports.map       = map
+module.exports = { each: each
+                 , at: at
+                 , put: put
+                 , remove: remove
+                 , clear: clear
+                 , size: size
+                 , empty_p: empty_p
+                 , has_key_p: has_key_p
+                 , keys: keys
+                 , values: values
+                 , items: items
+                 , reduce: reduce
+                 , every: every
+                 , some: some
+                 , filter: filter
+                 , map: map
+                 }
