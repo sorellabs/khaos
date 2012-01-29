a69e37a0adcca18b0f3adcd16ee4145a2beac3b9
Quildreen Motta <quildreen@gmail.com>
2012-01-29 21:02:18 -0200

    Updates type module tests to use expect.js assertions.

--- test/type.js
+++ test/type.js
@@ -1,98 +1,98 @@
-var type = require('../src/type/core')
+describe('{} type.core', function() {
+  var type = require('../src/type/core')
+  var expect = require('expect.js')
 
-describe('Module: type', function() {
+  describe('λ is_p', function() {
+    it('Should check for instanceof relationships when given a Function', function() {
+      expect(type.is_p([], Array)).to.be.ok()
+      expect(type.is_p([], Object)).to.be.ok()
+      expect(type.is_p({}, Array)).to.not.be.ok() })
 
-  describe('is_p', function() {
-    it('- Should check for instanceof relationships when given a Function', function() {
-      type.is_p([], Array).should.be.true
-      type.is_p([], Object).should.be.true
-      type.is_p({}, Array).should.be.false })
-
-    it('- Should check for the object in the [[Prototype]] chain when given an Object', function() {
-      type.is_p([], Array.prototype).should.be.true
-      type.is_p([], Object.prototype).should.be.true
-      type.is_p({}, Array.prototype).should.be.false })
+    it('Should check for the object in the [[Prototype]] chain when given an Object', function() {
+      expect(type.is_p([], Array.prototype)).to.be.ok()
+      expect(type.is_p([], Object.prototype)).to.be.ok()
+      expect(type.is_p({}, Array.prototype)).to.not.be.ok() })
   })
 
-  describe('name', function() {
-    it('- Should return the [[Class]] of the given subject', function() {
-      type.name([]).should.equal('Array')
-      type.name({}).should.equal('Object')
-      type.name(function(){}).should.equal('Function')
-      type.name(0).should.equal('Number')
-      type.name(NaN).should.equal('Number')
-      type.name('').should.equal('String')
-      type.name(new Date).should.equal('Date')
-      type.name(undefined).should.equal('Undefined')
-      type.name(null).should.equal('Null')
-      type.name(true).should.equal('Boolean')
-      type.name(/re/).should.equal('RegExp')
-      type.name(new Boolean(true)).should.equal('Boolean')
-      type.name(new String('')).should.equal('String')
-      type.name(new Number(0)).should.equal('Number') })
+  describe('λ name', function() {
+    it('Should return the [[Class]] of the given subject', function() {
+      expect(type.name([])).to.eql('Array')
+      expect(type.name({})).to.eql('Object')
+      expect(type.name(function(){})).to.eql('Function')
+      expect(type.name(0)).to.eql('Number')
+      expect(type.name(NaN)).to.eql('Number')
+      expect(type.name('')).to.eql('String')
+      expect(type.name(new Date)).to.eql('Date')
+      expect(type.name(undefined)).to.eql('Undefined')
+      expect(type.name(null)).to.eql('Null')
+      expect(type.name(true)).to.eql('Boolean')
+      expect(type.name(/re/)).to.eql('RegExp')
+      expect(type.name(new Boolean(true))).to.eql('Boolean')
+      expect(type.name(new String(''))).to.eql('String')
+      expect(type.name(new Number(0))).to.eql('Number') })
   })
 
-  describe('string_p', function() {
-    it('- Should check if the subject is-a plain String', function() {
-      type.string_p('').should.be.true
-      type.string_p(new String('')).should.be.true
-      type.string_p(['']).should.be.false })
+  describe('λ string_p', function() {
+    it('Should check if the subject is-a plain String', function() {
+      expect(type.string_p('')).to.be.ok()
+      expect(type.string_p(new String(''))).to.be.ok()
+      expect(type.string_p([''])).to.not.be.ok() })
   })
 
-  describe('number_p', function() {
-    it('- Should check if the subject is-a plain Number', function() {
-      type.number_p(0).should.be.true
-      type.number_p(NaN).should.be.true
-      type.number_p('0').should.be.false })
+  describe('λ number_p', function() {
+    it('Should check if the subject is-a plain Number', function() {
+      expect(type.number_p(0)).to.be.ok()
+      expect(type.number_p(NaN)).to.be.ok()
+      expect(type.number_p('0')).to.not.be.ok() })
   })
 
-  describe('date_p', function() {
-    it('- Should check if the subject is-a Date', function() {
-      type.date_p(new Date).should.be.true
-      type.date_p(0).should.be.false })
+  describe('λ date_p', function() {
+    it('Should check if the subject is-a Date', function() {
+      expect(type.date_p(new Date)).to.be.ok()
+      expect(type.date_p(0)).to.not.be.ok() })
   })
 
-  describe('regexp_p', function() {
-    it('- Should check if the subject is-a Regular Expression', function() {
-      type.regexp_p(/re/).should.be.true
-      type.regexp_p(new RegExp('re')).should.be.true
-      type.regexp_p('re').should.be.false })
+  describe('λ regexp_p', function() {
+    it('Should check if the subject is-a Regular Expression', function() {
+      expect(type.regexp_p(/re/)).to.be.ok()
+      expect(type.regexp_p(new RegExp('re'))).to.be.ok()
+      expect(type.regexp_p('re')).to.not.be.ok() })
   })
 
-  describe('fun_p', function() {
-    it('- Should check if the subject is-a Function', function() {
-      type.fun_p(function(){}).should.be.true
-      type.fun_p(new Function).should.be.true
-      type.fun_p(/re/).should.be.false })
+  describe('λ fun_p', function() {
+    it('Should check if the subject is-a Function', function() {
+      expect(type.fun_p(function(){})).to.be.ok()
+      expect(type.fun_p(new Function)).to.be.ok()
+      expect(type.fun_p(/re/)).to.not.be.ok() })
   })
 
-  describe('bool_p', function() {
-    it('- Should check if the subject is-a Boolean', function() {
-      type.bool_p(true).should.be.true
-      type.bool_p(false).should.be.true
-      type.bool_p(new Boolean(true)).should.be.true
-      type.bool_p(0).should.be.false })
+  describe('λ bool_p', function() {
+    it('Should check if the subject is-a Boolean', function() {
+      expect(type.bool_p(true)).to.be.ok()
+      expect(type.bool_p(false)).to.be.ok()
+      expect(type.bool_p(new Boolean(true))).to.be.ok()
+      expect(type.bool_p(0)).to.not.be.ok() })
   })
 
-  describe('object_p', function() {
-    it('- Should check if something is-a Object', function() {
-      type.object_p({}).should.be.true
-      type.object_p([]).should.be.false
-      type.object_p(null).should.be.false })
+  describe('λ object_p', function() {
+    it('Should check if something is-a Object', function() {
+      expect(type.object_p({})).to.be.ok()
+      expect(type.object_p([])).to.not.be.ok()
+      expect(type.object_p(null)).to.not.be.ok() })
   })
 
-  describe('primitive_p', function() {
-    it('- Should check if something is a primitive', function() {
-      type.primitive_p(1).should.be.true
-      type.primitive_p('').should.be.true
-      type.primitive_p(false).should.be.true
-      type.primitive_p(new Number(1)).should.be.false })
+  describe('λ primitive_p', function() {
+    it('Should check if something is a primitive', function() {
+      expect(type.primitive_p(1)).to.be.ok()
+      expect(type.primitive_p('')).to.be.ok()
+      expect(type.primitive_p(false)).to.be.ok()
+      expect(type.primitive_p(new Number(1))).to.not.be.ok() })
   })
 
-  describe('undefined_p', function() {
-    it('- Should check if something is undefined', function() {
-      type.undefined_p(undefined).should.be.true
-      type.undefined_p(null).should.be.false })
+  describe('λ undefined_p', function() {
+    it('Should check if something is undefined', function() {
+      expect(type.undefined_p(undefined)).to.be.ok()
+      expect(type.undefined_p(null)).to.not.be.ok() })
   })
 })
 
