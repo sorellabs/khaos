788d8cd2cc06c0b25ec04cc18b9498e513784035
Quildreen Motta <quildreen@gmail.com>
2012-05-01 15:15:02 -0300

    Adds tests for functional/async module.

---
+++ test/functional.async.js
@@ -0,0 +1,19 @@
+describe('{} functional.async', function() {
+  var _      = require('../src/functional/async')
+  var ensure = require('frame').ensure
+
+  describe('λ delay', function() {
+    it('Should execute `f` after the given `seconds`.', function(done) {
+      var start = new Date
+      _.delay(1, function() { var delta = (new Date - start) / 1000
+                              ensure(delta).above(1)
+                              done() })
+    })})
+
+  describe('λ defer', function() {
+    it('should asynchronously call `f` as soon as possible.', function(done) {
+      _.defer(function(){ ensure(async).ok()
+                          done() })
+      var async = true
+    })})
+})
